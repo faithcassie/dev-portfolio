6dbed2b488d5c1a98eabba34d94c4e2135309e18
Faith Nguyen <faithc.nguyen@gmail.com>
2023-09-13 18:28:57 -0500

    fix about page layout on mobile

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,15 +5,15 @@ import { navVariants, heroVariants, slideIn } from "../utils/motion";
 
 const AboutPage = () => {
   return (
-    <div className="relative h-full">
-      <div className="w-full h-full flex flex-row">
+    <div className="relative h-full pt-0 pb-28">
+      <div className="w-full h-full flex flex-col md:flex-row">
         <motion.img
           initial="hidden"
           animate="show"
           variants={heroVariants(0.2)}
           src={blossomGoldfish}
           alt="blossom-goldfish"
-          className="w-full md:w-[50%]  top-0"
+          className="w-full md:w-[50%] top-0"
         />
         <div className="w-[90%] h-full md:w-[40%]  pl-10 my-auto">
           <motion.h1
@@ -49,7 +49,7 @@ const AboutPage = () => {
             variants={heroVariants(0.5)}
           >
             During my college years, I discovered the beauty of web development
-            that completed my desire to transform ideas into real world
+            that completed my desire of transforming ideas into real world
             products. So, I decided to{" "}
             <span className=" text-[var(--primary)] font-semibold">
               switch my career path
@@ -62,7 +62,7 @@ const AboutPage = () => {
             variants={heroVariants(0.6)}
           >
             It brings joy and sense of fulfillment to me when collaborating with
-            others and crafting{" "}
+            others to craft{" "}
             <span className=" text-[var(--primary)] font-semibold">
               beautiful and engaging experiences
             </span>{" "}
